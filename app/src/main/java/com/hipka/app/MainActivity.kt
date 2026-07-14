78effd54dbd262550e39bc4e8ceb0110ec09431a
kiana <kiana.sherafati@gmail.com>
2026-07-15 02:18:51 +0330

    Fix: Resolve Hilt injection crash in MainActivity and add Bluetooth permissions

--- app/src/main/java/com/hipka/app/MainActivity.kt
+++ app/src/main/java/com/hipka/app/MainActivity.kt
@@ -12,31 +12,15 @@ import com.hipka.app.presentation.main.MainViewModel
 import com.hipka.app.presentation.navigation.HipkaNavGraph
 import com.hipka.app.presentation.theme.HipkaTheme
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
-import com.hipka.app.data.remote.api.SongApi
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject lateinit var songApi: SongApi //temporary for test supabase
 
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //supabase test // parand
-        lifecycleScope.launch {
-            try {
-                val songs = songApi.testGetSongs()
-                android.util.Log.d("SUPABASE_TEST", "اتصال موفقیت‌آمیز بود! تعداد آهنگ‌ها: ${songs.size}")
-            } catch (e: Exception) {
-                // ریزِ خطاهای جاوا و شبکه چاپ شود
-                android.util.Log.e("SUPABASE_TEST", "خطا در اتصال", e)
-            }
-        }
         setContent {
             val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
